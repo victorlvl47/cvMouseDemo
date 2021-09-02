07543d31aa8eec51798ab199481b49fcfa72ac78
victor <victor.lvl47@gmail.com>
2021-09-02 01:41:17 -0600

    added a bounding box

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -37,6 +37,9 @@ class handDetector():
 
     def findPosition(self, img, handNo=0, draw=True):
 
+        xList = []
+        yList = []
+        bbox = []
         self.lmList = []
 
         if self.results.multi_hand_landmarks:
@@ -45,12 +48,24 @@ class handDetector():
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (0, 0, 255),
                             cv2.FILLED)
 
-        return self.lmList
+        # Reiterate later!
+        # default=0 is a hack for when is no hand on sight. 
+        xmin, xmax = min(xList, default=0), max(xList, default=0)
+        ymin, ymax = min(yList, default=0), max(yList, default=0)
+        bbox = xmin, ymin, xmax, ymax
+
+        if draw:
+            cv2.rectangle(img, (xmin - 20, ymin - 20), 
+                (xmax + 20, ymax + 20), (0, 255, 0), 2)
+
+        return self.lmList, bbox
 
     def fingersUp(self):
         fingers = []
--- cvMouse.py
+++ cvMouse.py
@@ -10,8 +10,22 @@ cap = cv2.VideoCapture(0)
 cap.set(3, widthCam)
 cap.set(4, heightCam)
 
+pTime = 0
+
+detector = htm.handDetector(maxHands=1)
+
 while True:
     success, img = cap.read()
+
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    # fps
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), 
+        cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
